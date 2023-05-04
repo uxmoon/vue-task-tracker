a0631f9ae620b751b8deab1b551622ba0cb762f9
uxmoon <uxmoon@gmail.com>
2023-05-04 16:56:14 -0300

    Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Copyright &copy; 2023</p>
+    <!-- <a href="/about">About</a> -->
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+footer {
+  text-align: center;
+  margin-top: 2rem;
+}
+a {
+  color: #333;
+}
+</style>
